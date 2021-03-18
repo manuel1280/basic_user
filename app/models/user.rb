b83e28ea45a760d37b8880f72ab39161b2d2268c
manuel1280 <komodocr7@gmail.com>
2021-03-18 04:53:24 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@
 #
 class User < ApplicationRecord
   enum gender: { male: 0, female: 1 }
+
+  validates :birthday, presence: true
+  validates :full_name, presence: true, length: {minimum: 4, maximum: 20}, uniqueness: true
+  validates :image_url, presence: true, length: {minimum: 10}
+  validates :gender, presence: true
 end
